e7c705915158bd860348a79202ae2b9e5f515236
Joseph Manning <jgmanning3@yahoo.com>
2021-06-22 16:09:37 -0700

    Create employee test. Start build prompts in index.js

---
+++ _test_/Employee.test.js
@@ -0,0 +1,36 @@
+const Employee = require("../lib/Employee");
+
+describe("Employee", () => {
+    describe("Initilization", () => {
+        it("should create an object containing a name, email, and id", () => {
+            // Arrange
+            const employee = new Employee("Nick", "n@2u.com", 1);
+            // Act?
+            // Assert
+            expect("name" in employee).toBe(true);
+            expect("email" in employee).toBe(true);
+            expect("id" in employee).toBe(true);
+        });
+        
+        it("should set the name property via the constructor arguments", () => {
+            // Arrange
+            const employee = new Employee("Nick", "n@2u.com", 1);
+            // Assert
+            expect(employee.name).toBe("Nick");
+        });
+
+        it("should set the email property via the constructor arguments", () => {
+            // Arrange
+            const employee = new Employee("Nick", "n@2u.com", 1);
+            // Assert
+            expect(employee.email).toBe("n@2u.com");
+        });
+
+        it("should set the id property via the constructor arguments", () => {
+            // Arrange
+            const employee = new Employee("Nick", "n@2u.com", 1);
+            // Assert
+            expect(employee.id).toBe(1);
+        });
+    });
+});
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-var inquirer = require("inquirer");
+const inquirer = require("inquirer");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
 
 
 inquirer
@@ -21,62 +24,87 @@ inquirer
     {
       type: "input",
       message: "What is the team manager's office number?",
-      name: "managerOffice",
+      name: "officeNumber",
     },
-    {
-      type: "list",
-      message: "What type of employee profile would you like to add next?",
-      name: "subordinateChoice",
-      choices: ["Engineer", "Intern", "None"],
-    },
-
+    
     // Create if statements to proceed to engineer or intern questions
-
+    
     // Enginner questions
     {
         type: "input",
         message: "What is the engineer's name?",
         name: "engineerName",
-      },
-      {
+    },
+    {
         type: "input",
         message: "What is the engineer's employee ID?",
         name: "engineerID",
-      },
-      {
+    },
+    {
         type: "input",
         message: "What is the engineer's email address?",
         name: "engineerEmail",
-      },
-      {
+    },
+    {
         type: "input",
         message: "What is the engineer's GitHub account?",
         name: "engineerGithub",
-      },
-
+    },
+    
     //  Loop back to subordinateChoice
     
     //   Intern questions
-      {
+    {
         type: "input",
         message: "What is the intern's name?",
         name: "internName",
-      },
-      {
+    },
+    {
         type: "input",
         message: "What is the intern's employee ID?",
         name: "internID",
-      },
-      {
+    },
+    {
         type: "input",
         message: "What is the intern's email address?",
         name: "internEmail",
-      },
-      {
+    },
+    {
         type: "input",
         message: "What is the intern's school?",
         name: "internSchool",
-      },
-
+    },
+    
     //   Loop back to subordinate choice
-  ])
+    
+])
+.then(function (manager) {
+    console.log(manager);
+    employees.push(
+        new Manager(
+            manager.name,
+            manager.email,
+            manager.id,
+            manager.officeNumber
+            )
+            );
+            console.log(employee);
+        });
+        
+        function employeeMenu() {
+            inquirer.prompt({
+                type: "list",
+                message: "What type of employee profile would you like to add next?",
+                name: "employeeChoice",
+                choices: ["Engineer", "Intern", "None"],
+})
+.then(function (data) {
+    if (data.employeechoice === "Engineer") {
+        buildEngineer();
+    } else if (data.employeeChoice === "Intern") {
+        buildIntern();
+    } else {
+        // create our html page
+    }
+}),
+        }
